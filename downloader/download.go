80afe256620c560220df343d5856895f265ee631
Meagan <gouldma@g.cofc.edu>
2021-07-14 13:27:49 -0400

    adds an example of pipelines pattern for concurrency to download the urls

--- downloader/download.go
+++ downloader/download.go
@@ -5,59 +5,104 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 )
 
-type WebsiteDownloader struct{
-	UrlList []string
-	StorageLocation string 
+type WebsiteDownloader struct {
+	UrlList         []string
+	StorageLocation string
 }
 
-func NewWebsiteDownloader(urlList []string, storageLocation string) *WebsiteDownloader{
-	return &WebsiteDownloader{UrlList: urlList, StorageLocation: storageLocation} 
+func NewWebsiteDownloader(urlList []string, storageLocation string) *WebsiteDownloader {
+	return &WebsiteDownloader{UrlList: urlList, StorageLocation: storageLocation}
 }
 
 func (wd *WebsiteDownloader) Run() {
 	// constraint: this needs to be concurrent
-	// create a method to download 
+	// create a method to download
 	// create a method to store the site in a location
 
-	output, err := downloadSite("https://google.com")
-	if err != nil {
-		panic(err)
-	}
-
-	err = saveSite(output, "./temp/googledotcom.html")
-	if err != nil {
-		fmt.Println(err)
-	}
+	// put the urls into a channel
+	urlChannel := wd.outputUrls()
+	// grab those  vars from a channel and download them concurrently
+	wd.downloadUrls(urlChannel)
 }
 
-func downloadSite(url string) (*bytes.Buffer, error){
-	 resp, err := http.Get(url)
-	 if err != nil {
-		return nil, err 
-	 }
-	 // put the response in a buffer
-	 defer resp.Body.Close()
-	 buffer := bytes.NewBuffer(nil)
-	 io.Copy(buffer, resp.Body)
 
-	 return buffer, nil 
+
+// downloads a site a site
+// returns a bytes.buffer and error 
+func downloadSite(url string) (*bytes.Buffer, error) {
+	// run a get request for the website
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	// put the response in a buffer
+	defer resp.Body.Close()
+	buffer := bytes.NewBuffer(nil)
+	io.Copy(buffer, resp.Body)
+
+	// return the buffer and nil since it was successful 
+	return buffer, nil
 }
 
-func saveSite(downloadedSite *bytes.Buffer, saveLocation string) error{
-	file, err := os.Create(saveLocation)
-	if err != nil {
-		return err 
+func (wd *WebsiteDownloader) outputUrls() <- chan string{
+	urlChannel := make(chan string, len(wd.UrlList))
+	defer close(urlChannel)
+	for _, url := range wd.UrlList{
+		urlChannel <- url 
 	}
-	defer file.Close()
+	return urlChannel
+}
 
-	
-	_, err = io.Copy(file, downloadedSite)
-	if err != nil {
-		return nil 
+func (wd *WebsiteDownloader) downloadUrls(urls <- chan string) error{
+	for url := range urls {
+		// download the site
+		downloadedSite, err := downloadSite(url)
+		if err != nil {
+			return err
+		}
+
+		locationString, err := createLocationString(url)
+		if err != nil {
+			return nil 
+		}
+		err = saveSite(downloadedSite, locationString)
+		if err != nil {
+			return nil 
+		}
 	}
 
 	return nil 
+}
+
+func createLocationString(urlString string) (string, error) {
+	fileUrl, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("./temp/%s.html", fileUrl.Host), nil 
+}
+
+// saves the site to a specified location 
+// take in a bytes bugger and a string to save the site too 
+// returns an error if any occur
+func saveSite(downloadedSite *bytes.Buffer, saveLocation string) error {
+	// create the file. The directory needs to be created first
+	file, err := os.Create(saveLocation)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// copy the bytes buffer into the downloaded site
+	_, err = io.Copy(file, downloadedSite)
+	if err != nil {
+		return err
+	}
+
+	// return nil since no error occurred
+	return nil
 }
--- main.go
+++ main.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"strings"
 
 	"github.com/mgould1799/WebsiteDownloader/downloader"
 )
 
 func main() {
-	fmt.Println("hi")
-	urlListString := flag.String("urls", "google.com,facebook.com", "a list of comma separated urls to download")
+	urlListString := flag.String("urls", "https://google.com,https://facebook.com", "a list of comma separated urls to download")
 	fileStorageLocation := flag.String("storageLocation", "/temp", "path to store the downloaded sites")
 	flag.Parse()
 
 	// parse the url list
-	// TODO: check if the string contains a , before splitting 
+	// TODO: check if the string contains a , before splitting
 	urlList := strings.Split(*urlListString, ",")
 
 	websiteDownloader := downloader.NewWebsiteDownloader(urlList, *fileStorageLocation)
