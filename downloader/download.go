faae4c0089613aea29737dd559e30854a1bf1335
Meagan <gouldma@g.cofc.edu>
2021-07-26 14:09:00 -0400

    controls the concurrency and checks for https on a string

--- downloader/download.go
+++ downloader/download.go
@@ -8,17 +8,20 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
+	"sync"
 )
 
 // a struct to host the storage location and urllist
 type WebsiteDownloader struct {
 	UrlList         []string
 	StorageLocation string
+	MaxDownloads    int
 }
 
 // a funciton to create a new object on the string
 func NewWebsiteDownloader(urlList []string, storageLocation string) *WebsiteDownloader {
-	return &WebsiteDownloader{UrlList: urlList, StorageLocation: storageLocation}
+	return &WebsiteDownloader{UrlList: urlList, StorageLocation: storageLocation, MaxDownloads: 2}
 }
 
 // run the actual downloading of the urls
@@ -48,6 +51,11 @@ func (wd *WebsiteDownloader) outputUrls() <-chan string {
 	urlChannel := make(chan string, len(wd.UrlList))
 	defer close(urlChannel)
 	for _, url := range wd.UrlList {
+		// add https:// to a url for incase it needs it
+		if !strings.Contains(url, "https://") {
+			temp := url
+			url = fmt.Sprintf("https://%s", temp)
+		}
 		urlChannel <- url
 	}
 	return urlChannel
@@ -55,39 +63,57 @@ func (wd *WebsiteDownloader) outputUrls() <-chan string {
 
 // downlaods the sites and saves them to a place
 // output response to channel
+// constraint on the number of go routines that can be ran
 func (wd *WebsiteDownloader) downloadUrls(urls <-chan string) <-chan Creation {
+	// create the output channel
 	output := make(chan Creation)
 
-	go func() {
-		defer close(output)
-		for url := range urls {
-			// download the site
-			downloadedSite, err := downloadSite(url)
-			if err != nil {
-				output <- *newCreation("", fmt.Sprintf("%v - error occurred while downloading site - %v", url, err))
-				break
-			}
-	
-			locationString, err := wd.createLocationString(url)
-			if err != nil {
-				output <- *newCreation("", fmt.Sprintf("%v - error occurred while creating location string - %v", url, err))
-				break
-			}
-			err = saveSite(downloadedSite, locationString)
-			if err != nil {
-				output <- *newCreation("", fmt.Sprintf("%v - error occurred while saving site - %v", url, err))
-				break
-			}
+	// create a wait group to control the number of workers
+	var wg sync.WaitGroup
+	// loop through the number of workers allowed
+	for i := 0; i < wd.MaxDownloads; i++ {
+		// say hey wait
+		wg.Add(1)
+		// span off on the go routine
+		go func() {
+			defer wg.Done()
+			for url := range urls {
+				// download the site
+				downloadedSite, err := downloadSite(url)
+				if err != nil {
+					output <- *newCreation("", fmt.Sprintf("%v - error occurred while downloading site - %v", url, err))
+					break
+				}
 
-			output <- *newCreation(fmt.Sprintf("successfully downloaded - %v - saved to %v", url, locationString), "")
-		}
+				// create the location to save the string
+				locationString, err := wd.createLocationString(url)
+				if err != nil {
+					output <- *newCreation("", fmt.Sprintf("%v - error occurred while creating location string - %v", url, err))
+					break
+				}
+				// save the site to specified location
+				err = saveSite(downloadedSite, locationString)
+				if err != nil {
+					output <- *newCreation("", fmt.Sprintf("%v - error occurred while saving site - %v", url, err))
+					break
+				}
+				// output to the channel
+				output <- *newCreation(fmt.Sprintf("successfully downloaded - %v - saved to %v", url, locationString), "")
+			}
+		}()
+
+	}
+
+	// clean up the output channel to return it
+	go func() {
+		wg.Wait()
+		close(output)
 	}()
 	return output
-
 }
 
-// check if the give directory exists if not create it 
-func (wd *WebsiteDownloader) checkDirectory() error{
+// check if the give directory exists if not create it
+func (wd *WebsiteDownloader) checkDirectory() error {
 	newpath := filepath.Join(".", wd.StorageLocation)
 	return os.MkdirAll(newpath, os.ModePerm)
 }
@@ -98,7 +124,7 @@ func (wd *WebsiteDownloader) createLocationString(urlString string) (string, err
 	if err != nil {
 		return "", err
 	}
-	return fmt.Sprintf("%s/%s.html", wd.StorageLocation, fileUrl.Host), nil 
+	return fmt.Sprintf("%s/%s.html", wd.StorageLocation, fileUrl.Host), nil
 }
 
 // saves the site to a specified location
@@ -122,7 +148,7 @@ func saveSite(downloadedSite *bytes.Buffer, saveLocation string) error {
 	return nil
 }
 
-// downloads a site a site
+// downloads a site
 // returns a bytes.buffer and error
 func downloadSite(url string) (*bytes.Buffer, error) {
 	// run a get request for the website
